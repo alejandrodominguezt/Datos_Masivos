3933dbdad14bdabb07bfe3e4fc4ab2898c16e5ea
alejandrodominguezt <60455774+alejandrodominguezt@users.noreply.github.com>
2020-02-22 14:12:33 -0800

    Update Evaluacion1.scala

--- Evaluaciones/Evaluacion1.scala
+++ Evaluaciones/Evaluacion1.scala
@@ -6,17 +6,17 @@
 
 val arr = Array(Array(11,2,4),Array(4,5,6),Array(10,8,-12));
 
-def diagonalDifference(arr:Array[Array[Int]], n:Int, rightSide:Boolean, leftSide: Boolean):Int=
+def diagonalDifference(arr:Array[Array[Int]], n:Int, diagonal_1:Boolean, diagonal_2: Boolean):Int=
 {
     var sum:Int=0
-   if (rightSide){
+   if (diagonal_1){
        for(i<-Range(0,n))
        {
            sum = sum + arr(i)(i)
        }
    }
    var sum2:Int=0
-   if (leftSide){
+   if (diagonal_2){
        for(i <- Range(0,n)){
            sum2 = sum2 + arr(i)((n-1)-i)
        }
