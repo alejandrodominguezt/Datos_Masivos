2736f194f8fe155f564fc4382530a91d40643b17
Saul Ivan Soto Pino <saulivan852@gmail.com>
2020-02-21 19:12:32 -0800

    Added Evaluacion made in class

---
+++ Evaluaciones/Evaluacion1.scala
@@ -0,0 +1,15 @@
+
+//Evaluacion1
+
+val arr = Array((11,2,4),(4,5,6),(10,8,-12));
+
+def diagonalDifference(array:Array[Int]):Int={
+    val sub1 = arr.head;
+    val d1d1 = sub1._1;
+    val sub2 = arr.tail;
+    val sub3 = sub2.head;
+    val d2d1 = sub3._2;
+    val sub4 = sub2.tail;
+    val d3d1 = sub4._3;
+
+}
