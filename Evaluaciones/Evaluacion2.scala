48d4c4f3fe83bb7ed7d5ceea942e55a04be154f2
alejandrodominguezt <60455774+alejandrodominguezt@users.noreply.github.com>
2020-02-29 12:32:27 -0800

    Update Evaluacion2.scala

--- Evaluaciones/Evaluacion2.scala
+++ Evaluaciones/Evaluacion2.scala
@@ -19,16 +19,38 @@ df.head(5)
 df.describe().show()
 
 //7
-val df2 = df.withColumn("HV Ratio",(df("High") / df("Volume")));
+val df2 = df.withColumn("HV Ratio",df("High")/df("Volume"))
+df2.show()
 
 //8
-df.select(max("Close")).show()
-df.select(day(df("Date"))).show()
+df.orderBy($"Close".desc).show(1)
 
 //9
-val total = df.filter($"High">0).count()
-val result = df.filter($"High">500).count()
-val porcenta = result * 100;
-val procentaje =  porcenta / total;
-val dohhh = procentaje  * 1.0;
+// "Close" column represents the closing price per day
+df.select(mean("Close")).show()
+
+//10
+df.select(max("Volume")).show()
+df.select(min("Volume")).show
+
+//11
+  //a. 
+df.filter($"Close"<600).count()
+
+  //b. 
+(df.filter($"High" > 500).count * 1.0 / df.count())*100
+
+  //c.
+df.select(corr("High", "Volume")).show()
+
+  //d.
+val ydf = df.withColumn("Year", year(df("Date")))
+val maxdf = ydf.select($"Year", $"High").groupBy("Year").max()
+maxdf.select($"Year", $"max(High)").orderBy("Year").show()
+
+  //e.
+val mdf = df.withColumn("Month", month(df("Date")))
+val promdf = mdf.select($"Month", $"Close").groupBy("Month").mean()
+promdf.select($"Month", $"avg(Close)").orderBy("Month").show()
+
 
