d5cab6921fcca292575db117e81c7d26de717f1d
saulsp96 <55709953+saulsp96@users.noreply.github.com>
2020-06-10 17:47:05 -0700

    Update Practice_8.scala

--- Tareas_Practicas/Practice_8.scala
+++ Tareas_Practicas/Practice_8.scala
@@ -1,34 +1,32 @@
-import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.classification.{LogisticRegression, OneVsRest}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
-// Load the data stored in LIBSVM format as a DataFrame.
-val data = spark.read.format("libsvm")
+// load data file.
+val inputData = spark.read.format("libsvm")
   .load("data/mllib/sample_multiclass_classification_data.txt")
 
-// Split the data into train and test
-val splits = data.randomSplit(Array(0.6, 0.4), seed = 1234L)
-val train = splits(0)
-val test = splits(1)
+// generate the train/test split.
+val Array(train, test) = inputData.randomSplit(Array(0.8, 0.2))
 
-// specify layers for the neural network:
-// input layer of size 4 (features), two intermediate of size 5 and 4
-// and output of size 3 (classes)
-val layers = Array[Int](4, 5, 4, 3)
+// instantiate the base classifier
+val classifier = new LogisticRegression()
+  .setMaxIter(10)
+  .setTol(1E-6)
+  .setFitIntercept(true)
 
-// create the trainer and set its parameters
-val trainer = new MultilayerPerceptronClassifier()
-  .setLayers(layers)
-  .setBlockSize(128)
-  .setSeed(1234L)
-  .setMaxIter(100)
+// instantiate the One Vs Rest Classifier.
+val ovr = new OneVsRest().setClassifier(classifier)
 
-// train the model
-val model = trainer.fit(train)
+// train the multiclass model.
+val ovrModel = ovr.fit(train)
 
-// compute accuracy on the test set
-val result = model.transform(test)
-val predictionAndLabels = result.select("prediction", "label")
+// score the model on test data.
+val predictions = ovrModel.transform(test)
+
+// obtain evaluator.
 val evaluator = new MulticlassClassificationEvaluator()
   .setMetricName("accuracy")
 
-println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
+// compute the classification error on test data.
+val accuracy = evaluator.evaluate(predictions)
+println(s"Test Error = ${1 - accuracy}")
